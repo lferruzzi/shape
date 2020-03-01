9473b9f9035c545490494302ee50c743dc3f1b52
lferruzzi <ferruzzi.luciano@gmail.com>
2020-03-01 19:59:38 -0300

    Add Basic Material Design Theme
    - Added Toolbar to the MainActivity

--- app/src/main/java/br/com/lferruzzi/shapetest/MainActivity.kt
+++ app/src/main/java/br/com/lferruzzi/shapetest/MainActivity.kt
@@ -1,5 +1,14 @@
 package br.com.lferruzzi.shapetest
 
-import androidx.fragment.app.FragmentActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.lferruzzi.shapetest.databinding.MainActivityBinding
 
-class MainActivity : FragmentActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: MainActivityBinding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        setSupportActionBar(binding.toolbar)
+    }
+}
